915b23259f962c831cb60b5cd8352ee5cabe1b70
Aaron <aaronborgi1@gmail.com>
2023-12-24 13:22:34 +0000

    bg fix numero cinque

--- components/sections/WelcomeSection.vue
+++ components/sections/WelcomeSection.vue
@@ -35,7 +35,7 @@
 /* Optional: Responsive adjustments for smaller screens */
 @media (max-width: 767px) {
 	#coven-welcome-section {
-		background-size: contain; /* Adjust size to better fit mobile screens */
+		background-size: cover; /* Adjust size to better fit mobile screens */
 		min-height: 100vh; /* Adjust minimum height for mobile screens */
 	}
 }
